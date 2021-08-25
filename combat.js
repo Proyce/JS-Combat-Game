019efaf739e2a1fce53919a452f2a1d3689e0524
Prosper Nnadi <prospernnadi3@gmail.com>
2021-08-26 00:11:09 +0100

    commit 5  Combat Game

--- combat.js
+++ combat.js
@@ -1,7 +1,45 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+    if(guess < 0 || guess > 6) {
+        alert('Please enter a number between 0 and 6')
+    } else {
+        guesses ++;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits++;
+            if (hits == 3) {
+            isSunk = true;
+            alert("You sank my battleship!");
+                }
+            } else {
+                alert("MISS!");
+            }
+    }
+}
+
+let stats = "You took " + guesses + " guesses to sink the battleship, " +
+"which means your shooting accuracy was " + (3/guesses) + " or " + ((3/guesses)*100).toFixed(2) + "%";
+alert(stats);
+document.write(stats);
+
+
+    // while (isSunk == false) {
+    //     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    //     if (guess < 0 || guess > 6) {
+    //     alert("Please enter a valid cell number!");
+    //     } else {
+    //     guesses = guesses + 1;
+    //     if (guess == location1 || guess == location2 || guess == location3) {
+    //     hits = hits + 1;
+    //     }
+    //     }
+    //     }
