303c576355c04b67481b17bb99c5e42dad12c8b7
Prosper Nnadi <prospernnadi3@gmail.com>
2021-08-27 12:31:59 +0100

    commit 6  Combat Game

--- combat.js
+++ combat.js
@@ -31,15 +31,3 @@ let stats = "You took " + guesses + " guesses to sink the battleship, " +
 // alert(stats);
 document.write(stats);
 
-
-    // while (isSunk == false) {
-    //     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-    //     if (guess < 0 || guess > 6) {
-    //     alert("Please enter a valid cell number!");
-    //     } else {
-    //     guesses = guesses + 1;
-    //     if (guess == location1 || guess == location2 || guess == location3) {
-    //     hits = hits + 1;
-    //     }
-    //     }
-    //     }
